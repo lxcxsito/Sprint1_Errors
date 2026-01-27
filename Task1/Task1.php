2d35e50dbdeb70bf356cb0c7837f99ee858f9595
Lucas <lxcxsito@gmail.com>
2026-01-27 10:36:32 +0100

    Arreglado el ejercicio

--- Task1/Task1.php
+++ Task1/Task1.php
@@ -1,17 +1,16 @@
 <?php
 
 //Task 1
-Dividir(10, 0);
+echo Dividir(10, 0);
 function Dividir($a, $b)
 {
     try {
         if ($b == 0) {
-            echo throw new ErrorException("No es pot dividir per 0" . "\n");
+            throw new Exception("No es pot dividir per 0" . "\n");
         }
         return $a / $b;
     } catch (Exception $e) {
-        echo $e . "\n";
-        return $e->getMessage();
+        return "Error: " . $e->getMessage();
     }
 
 }
